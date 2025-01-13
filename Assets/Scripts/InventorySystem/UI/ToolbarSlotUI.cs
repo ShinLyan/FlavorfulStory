9888d6ce8019cff748aa8b21f7d309af17d9ac62
Vladimir Shin <vashin@edu.hse.ru>
2025-01-13 18:17:16 +0300

    Добавлены комменты в тулбаре

--- Assets/Scripts/InventorySystem/UI/ToolbarSlotUI.cs
+++ Assets/Scripts/InventorySystem/UI/ToolbarSlotUI.cs
@@ -5,29 +5,27 @@ using UnityEngine.UI;
 
 namespace FlavorfulStory.InventorySystem.UI
 {
+    /// <summary> Слот панели инструментов для отображения предметов инвентаря. </summary>
     [RequireComponent(typeof(Image))]
     public class ToolbarSlotUI : MonoBehaviour, IItemHolder,
         IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
     {
-        /// <summary> ������ ����� ���������. </summary>
+        /// <summary> Отображение иконки предмета в слоте. </summary>
         [SerializeField] private InventoryItemIcon _icon;
 
-        /// <summary> ����� �������. </summary>
+        /// <summary> Текст с номером слота. </summary>
         [SerializeField] private TMP_Text _keyText;
 
-        /// <summary> ������ ����� � HUD. </summary>
+        /// <summary> Индекс текущего слота на панели. </summary>
         private int _index;
 
-        /// <summary>
-        /// 
-        /// </summary>
+        /// <summary> Панель инструментов, которой принадлежит слот. </summary>
         private Toolbar _toolbar;
 
-        /// <summary>
-        /// 
-        /// </summary>
+        /// <summary> Выбран ли слот? </summary>
         private bool _isSelected;
 
+        /// <summary> Инициализация индекса слота, текста номера и панели инструментов. </summary>
         private void Awake()
         {
             _index = transform.GetSiblingIndex();
@@ -35,46 +33,57 @@ namespace FlavorfulStory.InventorySystem.UI
             _toolbar = transform.parent.GetComponent<Toolbar>();
         }
 
+        /// <summary> Обновление содержимого слота на основе текущего состояния инвентаря. </summary>
         public void Redraw()
         {
             _icon.SetItem(Inventory.PlayerInventory.GetItemInSlot(_index), 
                 Inventory.PlayerInventory.GetNumberInSlot(_index));
         }
 
-        /// <summary> �������� �������, ������� � ������ ������ ��������� � ���� ���������. </summary>
-        /// <returns> ���������� �������, ������� � ������ ������ ��������� � ���� ���������. </returns>
+        /// <summary> Получение предмета, находящегося в текущем слоте. </summary>
+        /// <returns> Предмет, находящийся в текущем слоте. </returns>
         public InventoryItem GetItem() => Inventory.PlayerInventory.GetItemInSlot(_index);
 
+        /// <summary> Установка состояния слота как выбранного. </summary>
         public void Select()
         {
             _isSelected = true;
             FadeToColor(Color.white);
         }
 
+        /// <summary> Сброс состояния слота как выбранного. </summary>
         public void ResetSelection()
         {
             _isSelected = false;
             FadeToColor(Color.gray);
         }
+        
+        /// <summary> Изменение цвета слота с анимацией. </summary>
+        /// <param name="color"> Новый цвет слота. </param>
         private void FadeToColor(Color color)
         {
             const float FadeDuration = 0.2f;
             GetComponent<Image>().CrossFadeColor(color, FadeDuration, true, true);
         }
 
+        /// <summary> Обработка клика по слоту. </summary>
+        /// <param name="eventData"> Данные события клика. </param>
         public void OnPointerClick(PointerEventData eventData)
         {
             _toolbar.SelectItem(_index);
         }
 
+        /// <summary> Обработка наведения курсора на слот. </summary>
+        /// <param name="eventData"> Данные события наведения. </param>
         public void OnPointerEnter(PointerEventData eventData)
         {
             FadeToColor(Color.white);
         }
 
+        /// <summary> Обработка выхода курсора из слота. </summary>
+        /// <param name="eventData"> Данные события выхода. </param>
         public void OnPointerExit(PointerEventData eventData)
         {
-            //if (!_isSelected) FadeToColor(Color.white);
             if (!_isSelected) FadeToColor(Color.gray);
         }
     }
