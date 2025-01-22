8623ff4edbe5409d67e5809292225cf27c19cf6c
Vladimir Shin <vashin@edu.hse.ru>
2025-01-23 01:40:08 +0300

    Добавление комментариев. Изменение кодировки всех скриптов

--- Assets/Scripts/Enums.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public enum UserKeys
-{
-    PlayerInventory = KeyCode.B,
-    UseObject = KeyCode.E,
-    StartPlaceObject = KeyCode.F,
-    CommitPlaceObject = KeyCode.Mouse0,
-}
