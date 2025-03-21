c4bea6ba91c6c64d799eb356b24d6fd5edc33d5c
Vladimir Shin <vashin@edu.hse.ru>
2025-03-21 12:59:52 +0300

    Реализован редактор диалогов: DialogueEditor

--- Assets/Scripts/DialogueSystem/Dialogue.cs
+++ Assets/Scripts/DialogueSystem/Dialogue.cs
@@ -1,10 +1,119 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 
 namespace FlavorfulStory.DialogueSystem
 {
+    /// <summary> ScriptableObject, представляющий диалог. </summary>
     [CreateAssetMenu(menuName = "FlavorfulStory/Dialogue")]
-    public class Dialogue : ScriptableObject
+    public class Dialogue : ScriptableObject, ISerializationCallbackReceiver
     {
-        [SerializeField] private DialogueNode[] _nodes;
+        /// <summary> Узлы диалога. </summary>
+        [SerializeField] private List<DialogueNode> _nodes = new();
+
+        /// <summary> Словарь для быстрого поиска узлов по идентификатору. </summary>
+        private readonly Dictionary<string, DialogueNode> _nodeLookup = new();
+
+        /// <summary> Все узлы диалога. </summary>
+        public IEnumerable<DialogueNode> Nodes => _nodes;
+
+        /// <summary> Пересоздать словарь узлов при изменении ScriptableObject в редакторе. </summary>
+        private void OnValidate() => RebuildNodeLookup();
+
+        /// <summary> Действия перед сериализацией объекта. </summary>
+        public void OnBeforeSerialize()
+        {
+#if UNITY_EDITOR
+            if (_nodes.Count == 0) AddNode(CreateDialogueNode(null));
+
+            if (string.IsNullOrEmpty(AssetDatabase.GetAssetPath(this))) return;
+
+            foreach (var node in Nodes)
+                if (string.IsNullOrEmpty(AssetDatabase.GetAssetPath(node)))
+                    AssetDatabase.AddObjectToAsset(node, this);
+#endif
+        }
+
+        /// <summary> Действия после десериализации объекта. </summary>
+        public void OnAfterDeserialize()
+        {
+        }
+
+        /// <summary> Пересоздать словарь узлов. </summary>
+        private void RebuildNodeLookup()
+        {
+            _nodeLookup.Clear();
+            foreach (var node in Nodes)
+                if (node)
+                    _nodeLookup[node.name] = node;
+        }
+
+        /// <summary> Получить дочерние узлы указанного узла. </summary>
+        /// <param name="parentNode"> Родительский узел. </param>
+        /// <returns> Перечисление дочерних узлов. </returns>
+        public IEnumerable<DialogueNode> GetChildNodes(DialogueNode parentNode)
+        {
+            foreach (string childId in parentNode.ChildNodes)
+                if (_nodeLookup.TryGetValue(childId, out var childNode))
+                    yield return childNode;
+        }
+
+#if UNITY_EDITOR
+        /// <summary> Создать новый узел и добавить его к родителю. </summary>
+        /// <param name="parentNode"> Родительский узел. </param>
+        public void CreateNode(DialogueNode parentNode)
+        {
+            var newNode = CreateDialogueNode(parentNode);
+            Undo.RegisterCreatedObjectUndo(newNode, "Created Dialogue Node");
+            Undo.RecordObject(this, "Added Dialogue Node");
+            AddNode(newNode);
+        }
+
+        /// <summary> Создать новый узел. </summary>
+        /// <param name="parentNode"> Родительский узел. </param>
+        /// <returns> Новый узел диалога. </returns>
+        private static DialogueNode CreateDialogueNode(DialogueNode parentNode)
+        {
+            var newNode = CreateInstance<DialogueNode>();
+            newNode.name = Guid.NewGuid().ToString();
+
+            if (!parentNode) return newNode;
+
+            parentNode.AddChild(newNode.name);
+            newNode.SetPlayerSpeaking(!parentNode.IsPlayerSpeaking);
+
+            var newNodeOffset = new Vector2(250f, (parentNode.ChildNodes.Count - 1) * 100f);
+            newNode.SetPosition(parentNode.Rect.position + newNodeOffset);
+
+            return newNode;
+        }
+
+        /// <summary> Добавить узел в список и обновить словарь. </summary>
+        /// <param name="newNode"> Новый узел. </param>
+        private void AddNode(DialogueNode newNode)
+        {
+            _nodes.Add(newNode);
+            RebuildNodeLookup();
+        }
+
+        /// <summary> Удалить узел и обновить связи. </summary>
+        /// <param name="nodeToDelete"> Удаляемый узел. </param>
+        public void DeleteNode(DialogueNode nodeToDelete)
+        {
+            Undo.RecordObject(this, "Deleted Dialogue Node");
+            _nodes.Remove(nodeToDelete);
+            RebuildNodeLookup();
+            RemoveDanglingChildren(nodeToDelete);
+            Undo.DestroyObjectImmediate(nodeToDelete);
+        }
+
+        /// <summary> Удалить связи удаленного узла с другими узлами. </summary>
+        /// <param name="nodeToDelete"> Удаляемый узел. </param>
+        private void RemoveDanglingChildren(DialogueNode nodeToDelete)
+        {
+            foreach (var node in Nodes) node.RemoveChild(nodeToDelete.name);
+        }
+#endif
     }
 }
--- Assets/Scripts/DialogueSystem/DialogueNode.cs
+++ Assets/Scripts/DialogueSystem/DialogueNode.cs
@@ -1,13 +1,79 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace FlavorfulStory.DialogueSystem
 {
-    [Serializable]
-    public class DialogueNode
+    /// <summary> Узел диалога, содержащий текст, связи с дочерними узлами и параметры отображения. </summary>
+    public class DialogueNode : ScriptableObject
     {
-        [SerializeField] private string _uniqueId;
-        [SerializeField] private string _text;
-        [SerializeField] private string[] _children;
+        /// <summary> Говорит ли игрок в данном узле диалога? </summary>
+        [field: SerializeField]
+        public bool IsPlayerSpeaking { get; private set; }
+
+        /// <summary> Текст, отображаемый в узле диалога. </summary>
+        [field: SerializeField, TextArea(3, 5)]
+        public string Text { get; private set; } = "Dialogue Text";
+
+        /// <summary> Список идентификаторов дочерних узлов. </summary>
+        [field: SerializeField]
+        public List<string> ChildNodes { get; private set; } = new();
+
+        /// <summary> Позиция и размер узла в редакторе. </summary>
+        [field: SerializeField]
+        public Rect Rect { get; private set; } = new(0, 0, 200, 100);
+
+#if UNITY_EDITOR
+        /// <summary> Установить новую позицию узла в редакторе. </summary>
+        /// <param name="position"> Новая позиция узла. </param>
+        public void SetPosition(Vector2 position)
+        {
+            Undo.RecordObject(this, "Move Dialogue Node");
+
+            var rect = Rect;
+            rect.position = position;
+            Rect = rect;
+
+            EditorUtility.SetDirty(this);
+        }
+
+        /// <summary> Установить текст узла. </summary>
+        /// <param name="text"> Новый текст диалога. </param>
+        public void SetText(string text)
+        {
+            if (text == Text) return;
+
+            Undo.RecordObject(this, "Update Dialogue Text");
+            Text = text;
+            EditorUtility.SetDirty(this);
+        }
+
+        /// <summary> Добавить дочерний узел по его идентификатору. </summary>
+        /// <param name="childId"> Идентификатор дочернего узла. </param>
+        public void AddChild(string childId)
+        {
+            Undo.RecordObject(this, "Add Dialogue Link");
+            ChildNodes.Add(childId);
+            EditorUtility.SetDirty(this);
+        }
+
+        /// <summary> Удалить дочерний узел по его идентификатору. </summary>
+        /// <param name="childId"> Идентификатор дочернего узла. </param>
+        public void RemoveChild(string childId)
+        {
+            Undo.RecordObject(this, "Remove Dialogue Link");
+            ChildNodes.Remove(childId);
+            EditorUtility.SetDirty(this);
+        }
+
+        /// <summary> Установить, говорит ли в этом узле игрок. </summary>
+        /// <param name="isPlayerSpeaking"> `true`, если говорит игрок; иначе `false`. </param>
+        public void SetPlayerSpeaking(bool isPlayerSpeaking)
+        {
+            Undo.RecordObject(this, "Change Dialogue Speaker");
+            IsPlayerSpeaking = isPlayerSpeaking;
+            EditorUtility.SetDirty(this);
+        }
+#endif
     }
 }
