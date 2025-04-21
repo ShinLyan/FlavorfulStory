01195d0a832fe9328339edc441485d8c9321a18e
ArMaGeDoN <ilyakur025@gmail.com>
2025-04-21 21:04:39 +0300

    числа в константы

--- Assets/Scripts/Lighting/GlobalLightSystem.cs
+++ Assets/Scripts/Lighting/GlobalLightSystem.cs
@@ -29,6 +29,28 @@ namespace FlavorfulStory.Lightning
         /// <summary> Время появления луны (за 1 час до наступления ночи). </summary> 
         private const float MoonStartTime = NightStartTime - 1f;
 
+        /// <summary> Постоянное смещение теневого отображения. </summary>
+        private const float MinimalShadowBias = 0.01f;
+
+        /// <summary> Время выключения теней от солнца. </summary>
+        private const float OffSunShadowsHour = 17.5f;
+
+        /// <summary> Полдень. </summary>
+        private const float Midday = 0.5f;
+
+        /// <summary> Количество часов в сутках. </summary>
+        private const float HoursInDay = 24f;
+
+        /// <summary> Смещение, основанное на нормальном отображении теней. </summary>
+        private const float MoonShadowNormalBias = 0.4f;
+
+        private const float MoonAngleYStart = -45f;
+
+        private const float MoonAngleYEnd = 45f;
+
+        private const float MoonAngleX = 30f;
+
+
         /// <summary> Подписывается на событие обновления времени при активации. </summary> 
         private void OnEnable() => WorldTime.OnTimeUpdated += UpdateLighting;
 
@@ -73,11 +95,11 @@ namespace FlavorfulStory.Lightning
         private void SetSunShadows(float currentTimeInHours)
         {
             _sunLight.shadows = _currentWeatherLightSettings.SunShadowType;
-            _sunLight.shadowBias = 0.01f;
+            _sunLight.shadowBias = MinimalShadowBias;
 
-            if (currentTimeInHours >= 17.5f) // убираем тени если время 17:30 - 18:00
+            if (currentTimeInHours >= OffSunShadowsHour) // убираем тени если время 17:30 - 18:00
             {
-                float shadowProgress = Mathf.InverseLerp(17.5f, NightStartTime, currentTimeInHours);
+                float shadowProgress = Mathf.InverseLerp(OffSunShadowsHour, NightStartTime, currentTimeInHours);
                 _sunLight.shadowStrength =
                     Mathf.Lerp(_currentWeatherLightSettings.SunShadowStrength, 0f, shadowProgress);
             }
@@ -94,7 +116,7 @@ namespace FlavorfulStory.Lightning
             float sunAngleX;
             float sunAngleY;
 
-            if (sunProgress < 0.5f) // поворачиваем до 12:00
+            if (sunProgress < Midday) // поворачиваем до 12:00
             {
                 sunAngleX = Mathf.Lerp(_currentWeatherLightSettings.SunAngleX.x,
                     _currentWeatherLightSettings.SunAngleX.y, sunProgress);
@@ -135,8 +157,8 @@ namespace FlavorfulStory.Lightning
 
             SetMoonShadows(currentTimeInHours);
 
-            float moonProgress = Mathf.InverseLerp(MoonStartTime, 26f,
-                currentTimeInHours >= MoonStartTime ? currentTimeInHours : currentTimeInHours + 24f);
+            float moonProgress = Mathf.InverseLerp(MoonStartTime, HoursInDay + 2f,
+                currentTimeInHours >= MoonStartTime ? currentTimeInHours : currentTimeInHours + HoursInDay);
             RotateMoon(moonProgress);
             ColorizeMoon(moonProgress);
         }
@@ -146,17 +168,19 @@ namespace FlavorfulStory.Lightning
         private void SetMoonShadows(float currentTimeInHours)
         {
             _moonLight.shadows = _currentWeatherLightSettings.MoonShadowType;
-            _moonLight.shadowNormalBias = 0.4f;
+            _moonLight.shadowNormalBias = MoonShadowNormalBias;
+
+            float nightWithShadowTime = NightStartTime + 1f;
 
             if (DayStartTime < currentTimeInHours && currentTimeInHours < NightStartTime)
             {
                 // 17:00 - 18:00 теней от луны нету
                 _moonLight.shadowStrength = 0f;
             }
-            else if (currentTimeInHours >= NightStartTime && currentTimeInHours < 19f)
+            else if (currentTimeInHours >= NightStartTime && currentTimeInHours < nightWithShadowTime)
             {
                 // плавно включаем тени если время 18:00 - 19:00
-                float shadowProgress = Mathf.InverseLerp(NightStartTime, 19f, currentTimeInHours);
+                float shadowProgress = Mathf.InverseLerp(NightStartTime, nightWithShadowTime, currentTimeInHours);
                 _moonLight.shadowStrength =
                     Mathf.Lerp(0f, _currentWeatherLightSettings.MoonShadowStrength, shadowProgress);
             }
@@ -170,8 +194,8 @@ namespace FlavorfulStory.Lightning
         /// <param name="moonProgress"> Прогресс ночи от 0 до 1. </param>
         private void RotateMoon(float moonProgress)
         {
-            float moonAngleY = Mathf.Lerp(-45f, 45f, moonProgress);
-            _moonLight.transform.rotation = Quaternion.Euler(30f, moonAngleY, 0f);
+            float moonAngleY = Mathf.Lerp(MoonAngleYStart, MoonAngleYEnd, moonProgress);
+            _moonLight.transform.rotation = Quaternion.Euler(MoonAngleX, moonAngleY, 0f);
         }
 
         /// <summary> Изменяет цвет и интенсивность лунного света. </summary> 
